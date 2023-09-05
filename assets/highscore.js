ca5bd9432b28bb359458f0c5e040540a6c5ec066
acantu9 <adrianaacantu@gmail.com>
2023-09-04 19:10:35 -0500

    deleted highscore.html and highscore.js and removed highscore.html link in index.html

--- assets/highscore.js
+++
@@ -1,6 +0,0 @@
-var scores = JSON.parse(localStorage.getItem("score")) || [];
-scores.forEach(element => {
-    var score = document.createElement("p");
-    score.innerText = element.initials + " " + element.score;
-    document.querySelector(".highscore").appendChild(score);
-});
